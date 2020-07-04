5e1b632c94865113dd3d431c7fbf84dcaa68bae7
MohammedGhafri <eng.m.ghafri@gmail.com>
2020-07-04 03:18:34 +0300

    7

--- code-challenges/Regular-Expressions/challenges-04.test.js
+++ code-challenges/Regular-Expressions/challenges-04.test.js
@@ -129,6 +129,9 @@ For example, 'Welcome to Code 301!' will return 'W_lc_m_ t_ C_d_ 301!'.
 
 let hangman = (str) => {
   // Solution code here...
+  let regex=/a|e|i|o|u/gi;
+  return str.replace(regex,'_');
+
 };
 
 /* ------------------------------------------------------------------------------------------------
